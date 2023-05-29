b975090c8263351a09127d5795958f288451dd1d
Glebegor <glebegor2220@gmail.com>
2023-05-29 23:33:30 +0200

    Created empty router.

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	service "github.com/TonWork/back/pkg/service"
+	gin "github.com/gin-gonic/gin"
 )
 
 type Handler struct {
@@ -12,6 +13,14 @@ func NewHandler(services *service.Service) *Handler {
 	return &Handler{service: services}
 }
 
-// func (h *Handler) InitRoutes() *gin.Engine {
-// 	return
-// }
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	apiV2 := router.Group("/api/v2/")
+	{
+		auth := apiV2.Group("/auth")
+		{
+
+		}
+	}
+	return router
+}
