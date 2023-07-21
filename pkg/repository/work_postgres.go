ad623759c21ca5f0f10bbb8829f458697972c4a5
Glebegor <glebegor2220@gmail.com>
2023-07-21 20:35:38 +0200

    Work service created.

--- pkg/repository/work_postgres.go
+++ pkg/repository/work_postgres.go
@@ -18,9 +18,24 @@ func NewWorkPostgres(db *sql.DB) *WorkPostgres {
 func (r *WorkPostgres) GetAll() ([]TonWork.Work, error) {
 	var data []TonWork.Work
 	query := fmt.Sprintf("SELECT * FROM %s", Table_works)
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return data, err
+	}
+	defer rows.Close()
 
-	err := r.db.QueryRow(query).Scan(&data)
-	return data, err
+	for rows.Next() {
+		var work TonWork.Work
+		err := rows.Scan(&work.Id, &work.Title, &work.Description, &work.Text, &work.Tags, &work.Technologies, &work.Company, &work.Price, &work.ExperienceLevel, &work.Type_of_job, &work.Invites, &work.Rating)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, work)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 func (r *WorkPostgres) Create(userId int, data TonWork.Work) error {
 	tx, err := r.db.Begin()
@@ -28,14 +43,14 @@ func (r *WorkPostgres) Create(userId int, data TonWork.Work) error {
 		return err
 	}
 	var id int
-	createWorkQuery := fmt.Sprintf("INSERT INTO %s (title, description, text, tags, technologies, company, price, experienceLevel, type_of_job, invites, rating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id", Table_works)
-	row := tx.QueryRow(createWorkQuery, data.Title, data.Description, data.Text, data.Tags, data.Technologies, data.Company, data.Price, data.ExperienceLevel, data.Type_of_job, data.Invites, data.Rating)
+	createWorkQuery := fmt.Sprintf("INSERT INTO %s (title, description, text, tags, technologies, company, price, experienceLevel, type_of_job, invites, rating) OUTPUT inserted.id VALUES (@title, @description, @text, @tags, @technologies, @company, @price, @experienceLevel, @type_of_job, @invites, @rating)", Table_works)
+	row := tx.QueryRow(createWorkQuery, sql.Named("title", data.Title), sql.Named("description", data.Description), sql.Named("text", data.Text), sql.Named("tags", data.Tags), sql.Named("technologies", data.Technologies), sql.Named("company", data.Company), sql.Named("price", data.Price), sql.Named("experienceLevel", data.ExperienceLevel), sql.Named("type_of_job", data.Type_of_job), sql.Named("invites", data.Invites), sql.Named("rating", data.Rating))
 	if err := row.Scan(&id); err != nil {
 		tx.Rollback()
 		return err
 	}
-	createWorkUserQuery := fmt.Sprintf("INSERT INTO %s (id_user, id_works) VALUES ($1, $2)", Table_users_to_works)
-	_, err = tx.Exec(createWorkUserQuery, userId, id)
+	createWorkUserQuery := fmt.Sprintf("INSERT INTO %s (id_user, id_works) VALUES (@userId, @id)", Table_users_to_works)
+	_, err = tx.Exec(createWorkUserQuery, sql.Named("userId", userId), sql.Named("id", id))
 	if err != nil {
 		tx.Rollback()
 		return err
@@ -44,8 +59,8 @@ func (r *WorkPostgres) Create(userId int, data TonWork.Work) error {
 }
 func (r *WorkPostgres) GetById(id int) (TonWork.Work, error) {
 	var data TonWork.Work
-	query := fmt.Sprintf("SELECT * FROM %s WHERE id=$1", Table_works)
-	err := r.db.QueryRow(query, id).Scan(&data)
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id=@id", Table_works)
+	err := r.db.QueryRow(query, sql.Named("id", id)).Scan(&data.Id, &data.Title, &data.Description, &data.Text, &data.Tags, &data.Technologies, &data.Company, &data.Price, &data.ExperienceLevel, &data.Type_of_job, &data.Invites, &data.Rating)
 	return data, err
 }
 func (r *WorkPostgres) Update(id string, input TonWork.WorkUpdate) error {
@@ -53,48 +68,48 @@ func (r *WorkPostgres) Update(id string, input TonWork.WorkUpdate) error {
 	args := make([]interface{}, 0)
 	argId := 1
 	if input.Title != nil {
-		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
-		args = append(args, *input.Title)
+		setValues = append(setValues, fmt.Sprint("title=@Title"))
+		args = append(args, sql.Named("Title", *input.Title))
 		argId++
 	}
 	if input.Description != nil {
-		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
-		args = append(args, *input.Description)
+		setValues = append(setValues, fmt.Sprint("description=@Description"))
+		args = append(args, sql.Named("Description", *input.Description))
 		argId++
 	}
 	if input.Text != nil {
-		setValues = append(setValues, fmt.Sprintf("text=$%d", argId))
-		args = append(args, *input.Text)
+		setValues = append(setValues, fmt.Sprint("text=@Text"))
+		args = append(args, sql.Named("Text", *input.Text))
 		argId++
 	}
 	if input.Tags != nil {
-		setValues = append(setValues, fmt.Sprintf("tags=$%d", argId))
-		args = append(args, *input.Tags)
+		setValues = append(setValues, fmt.Sprint("tags=@Tags"))
+		args = append(args, sql.Named("Tags", *input.Tags))
 		argId++
 	}
 	if input.Technologies != nil {
-		setValues = append(setValues, fmt.Sprintf("technologies=$%d", argId))
-		args = append(args, *input.Technologies)
+		setValues = append(setValues, fmt.Sprint("technologies=@Technologies"))
+		args = append(args, sql.Named("Technologies", *input.Technologies))
 		argId++
 	}
 	if input.Company != nil {
-		setValues = append(setValues, fmt.Sprintf("company=$%d", argId))
-		args = append(args, *input.Company)
+		setValues = append(setValues, fmt.Sprint("company=@Company"))
+		args = append(args, sql.Named("Company", *input.Company))
 		argId++
 	}
 	if input.Price != nil {
-		setValues = append(setValues, fmt.Sprintf("price=$%d", argId))
-		args = append(args, *input.Price)
+		setValues = append(setValues, fmt.Sprint("price=@Price"))
+		args = append(args, sql.Named("Price", *input.Price))
 		argId++
 	}
 	if input.ExperienceLevel != nil {
-		setValues = append(setValues, fmt.Sprintf("experienceLevel=$%d", argId))
-		args = append(args, *input.ExperienceLevel)
+		setValues = append(setValues, fmt.Sprint("experienceLevel=@ExperienceLevel"))
+		args = append(args, sql.Named("ExperienceLevel", *input.ExperienceLevel))
 		argId++
 	}
 	if input.Type_of_job != nil {
-		setValues = append(setValues, fmt.Sprintf("type_of_job=$%d", argId))
-		args = append(args, *input.Type_of_job)
+		setValues = append(setValues, fmt.Sprint("type_of_job=@Type_of_job"))
+		args = append(args, sql.Named("Type_of_job", *input.Type_of_job))
 		argId++
 	}
 	setQuery := strings.Join(setValues, ", ")
@@ -104,7 +119,7 @@ func (r *WorkPostgres) Update(id string, input TonWork.WorkUpdate) error {
 	return err
 }
 func (r *WorkPostgres) Delete(id string) error {
-	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1", Table_works)
-	_, err := r.db.Exec(query, id)
+	query := fmt.Sprintf("DELETE FROM %s WHERE id=@id", Table_works)
+	_, err := r.db.Exec(query, sql.Named("id", id))
 	return err
 }
--- works.go
+++ works.go
@@ -1,18 +1,18 @@
 package TonWork
 
 type Work struct {
-	Title           string `json:"title" binding:"required"`
-	Description     string `json:"description" binding:"required"`
-	Text            string `json:"text" binding:"required"`
-	Tags            string `json:"tags" `
-	Technologies    string `json:"technologies" `
-	Company         string `json:"company" `
-	Price           int    `json:"price" binding:"required"`
-	ExperienceLevel string `json:"experienceLevel" binding:"required"`
-	Type_of_job     string `json:"type_of_job" `
-	Invites         int    `json:"invites" `
-	Rating          int    `json:"rating" binding:"required"`
 	Id              int    `json:"id" db:"id"`
+	Title           string `json:"title" binding:"required" db:"title"`
+	Description     string `json:"description" binding:"required" db:"description"`
+	Text            string `json:"text" binding:"required" db:"text"`
+	Tags            string `json:"tags" db:"tags"`
+	Technologies    string `json:"technologies" db:"technologies"`
+	Company         string `json:"company" db:"company"`
+	Price           int    `json:"price" binding:"required" db:"price"`
+	ExperienceLevel string `json:"experienceLevel" binding:"required" db:"experienceLevel"`
+	Type_of_job     string `json:"type_of_job" db:"type_of_job"`
+	Invites         int    `json:"invites" db:"invites"`
+	Rating          int    `json:"rating" binding:"required" db:"rating"`
 }
 type WorkUpdate struct {
 	Title           *string `json:"title"`
